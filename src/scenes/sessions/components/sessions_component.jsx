d6e739a722e438a6f34b7f8333c9e6710e0ef112
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2018-03-19 02:13:11 +0700

    validate the login form inputs
    the validation was done using the HTML5 Constraint validation, which is quite simple to use

--- src/scenes/sessions/components/sessions_component.jsx
+++ src/scenes/sessions/components/sessions_component.jsx
@@ -1,6 +1,27 @@
 import React, { Component } from "react";
 
 const SessionsComponent = props => {
+  const isValid = element => element.checkValidity();
+
+  const markTheInvalidInput = element => {
+    element.classList.add("input-invalid");
+    element.classList.remove("input-valid");
+  };
+
+  const markTheInputIsValid = element => {
+    element.classList.add("input-valid");
+  };
+
+  const validateInput = event => {
+    const { target } = event;
+    if (!isValid(target)) {
+      target.focus();
+      return markTheInvalidInput(target);
+    }
+
+    return markTheInputIsValid(target);
+  };
+
   return (
     <section>
       <form>
@@ -10,6 +31,7 @@ const SessionsComponent = props => {
           name="email"
           id="email"
           onChange={props.handleInputChange}
+          onBlur={validateInput}
           required
         />
 
@@ -19,6 +41,8 @@ const SessionsComponent = props => {
           name="password"
           id="password"
           onChange={props.handleInputChange}
+          onBlur={validateInput}
+          minLength={5}
           required
         />
 
