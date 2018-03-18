f5e2aadbebcd837860edab8fd06ccf8aa62b6f34
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2018-03-19 01:22:19 +0700

    update the login form
    - wrap the inputs inside the form element
    - use button element instead of p element to represent the `Submit Login` button

--- src/scenes/sessions/components/sessions_component.jsx
+++ src/scenes/sessions/components/sessions_component.jsx
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 const SessionsComponent = props => {
   return (
     <section>
-      <label htmlFor="email">Email</label>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        onChange={props.handleInputChange}
-      />
+      <form>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          onChange={props.handleInputChange}
+          required
+        />
 
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        name="password"
-        id="password"
-        onChange={props.handleInputChange}
-      />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={props.handleInputChange}
+          required
+        />
 
-      <p onClick={props.submit_login}>Submit Login</p>
-      <p>{props.session.id}</p>
+        <button type="submit" onClick={props.submit_login}>
+          Login
+        </button>
+      </form>
     </section>
   );
 };
