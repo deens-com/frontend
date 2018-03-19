e8bb23f22bc675799c33cf3904c5a44bb1fed7e5
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2018-03-19 21:33:37 +0700

    add react semantic UI
    This changes added the files generated by the semantic-ui and place it in styles directory.
    
    If the `gulp build` executed in the project root directory, it will re-compiles the changes and output it to `vendor` directory
    
    I'm not sure yet if this is the perfect approach to manage the semantic-ui related files, but let's keep it that way for now while searching for the best options that fit to our needs down the road.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './main/app';
-import registerServiceWorker from './registerServiceWorker';
-import Parse from 'parse';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./vendor/semantic.css";
+import "./index.css";
+import App from "./main/app";
+import registerServiceWorker from "./registerServiceWorker";
+import Parse from "parse";
 
-Parse.initialize("myAppId")
-Parse.serverURL = process.env.REACT_APP_PARSE_SERVER_URL || "http://api.please.docker/parse";
+Parse.initialize("myAppId");
+Parse.serverURL =
+  process.env.REACT_APP_PARSE_SERVER_URL || "http://api.please.docker/parse";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
