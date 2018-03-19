3f05701b563c553a79672cca00dac4276f60cb0a
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2018-03-19 23:41:55 +0700

    remove the unused style tag

--- src/scenes/sessions/components/login_form_component.jsx
+++ src/scenes/sessions/components/login_form_component.jsx
@@ -16,18 +16,6 @@ const LoginFormComponent = props => {
   return (
     <section>
       <div className="login-form">
-        {/*
-      Heads up! The styles below are necessary for the correct render of this example.
-      You can do same with CSS, the main idea is that all the elements up to the `Grid`
-      below must have a height of 100%.
-    */}
-        <style>{`
-      body > div,
-      body > div > div,
-      body > div > div > div.login-form {
-        height: 100%;
-      }
-    `}</style>
         <Grid
           textAlign="center"
           style={{ height: "100%" }}
