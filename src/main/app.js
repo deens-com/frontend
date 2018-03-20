b0beaacdfb308685d9dc2cf0da6b58072e228acd
Olivier Ducroux <olivier@dmates.io>
2018-03-20 10:36:48 +0100

    gh-pages config

--- src/main/app.js
+++ src/main/app.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import { Switch } from 'react-router'
-import { Router, Route, Layout } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './store'
-import history from './history'
-import ReactGA from 'react-ga'
-import withTracker from './middlewares/with_tracker'
-import Home from './../scenes/home/home'
-import Sessions from './../scenes/sessions/sessions'
-import Registrations from './../scenes/registrations/registrations'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Switch } from "react-router";
+import { Router, Route, Layout } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import history from "./history";
+import ReactGA from "react-ga";
+import withTracker from "./middlewares/with_tracker";
+import Home from "./../scenes/home/home";
+import Sessions from "./../scenes/sessions/sessions";
+import Registrations from "./../scenes/registrations/registrations";
 
 const App = () => {
-  return(
+  return (
     <Provider store={store}>
       <Router history={history}>
         <Switch>
-          <Route exact path={process.env.PUBLIC_URL + '/'} component={withTracker(Home)} />
-          <Route path='/login' component={withTracker(Sessions)} />
-          <Route path='/register' component={withTracker(Registrations)} />
+          <Route
+            exact
+            path={process.env.PUBLIC_URL + "/"}
+            component={withTracker(Home)}
+          />
+          <Route path="/login" component={withTracker(Sessions)} />
+          <Route path="/register" component={withTracker(Registrations)} />
         </Switch>
       </Router>
     </Provider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
