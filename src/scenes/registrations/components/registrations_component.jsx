39b6d9327cfba3c75484137fb253816fa4f9e8d2
Olivier Ducroux <olivier@dmates.io>
2018-03-21 17:26:43 +0100

    Registration feature styled and with proper logic

--- src/scenes/registrations/components/registrations_component.jsx
+++ src/scenes/registrations/components/registrations_component.jsx
@@ -1,17 +1,117 @@
 import React, { Component } from "react";
-import RegistrationFormComponent from "./registration_form_component";
+//import RegistrationFormComponent from "./registration_form_component";
 import { Page, PageContent } from "./../../../shared_components/layout/Page";
 import TopBar from "./../../../shared_components/TopBar";
 import BrandFooter from "./../../../shared_components/BrandFooter";
+import PropTypes from "prop-types";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Message,
+  Segment
+} from "semantic-ui-react";
+import { Logo } from "./../../../shared_components/icons";
+import { Link } from "react-router-dom";
 
 const RegistrationsComponent = props => {
   return (
-    <Page topPush>
-      <TopBar fixed withPadding />
-      <PageContent>
-        <RegistrationFormComponent {...props} />
-      </PageContent>
-    </Page>
+    <section>
+      <div className="login-form">
+        <Grid
+          textAlign="center"
+          style={{ height: "100%" }}
+          verticalAlign="middle"
+        >
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as="h2" color="teal" textAlign="center">
+              <Logo />
+              Register
+            </Header>
+            {Object.keys(props.errors).length !== 0 && (
+              <Message style={{ color: "red" }}>
+                {props.errors.error.message}
+              </Message>
+            )}
+            {Object.keys(props.state_errors).length !== 0 && (
+              <Message style={{ color: "red" }}>
+                {props.state_errors.message}
+              </Message>
+            )}
+            <Form size="large">
+              <Segment stacked>
+                <Form.Input
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="Username"
+                  type="text"
+                  name="username"
+                  id="username"
+                  onChange={props.handleInputChange}
+                  autoFocus
+                  required
+                />
+                <Form.Input
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="E-mail address"
+                  type="email"
+                  name="email"
+                  id="email"
+                  onChange={props.handleInputChange}
+                  onBlur={props.validateInput}
+                  error={props.isInputInvalid("email")}
+                  required
+                />
+                <Form.Input
+                  fluid
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Password"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={props.handleInputChange}
+                  onBlur={props.validateInput}
+                  error={props.isInputInvalid("password")}
+                  minLength={8}
+                  required
+                />
+                <Form.Input
+                  fluid
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Password confirmation"
+                  type="password"
+                  name="password_confirmation"
+                  id="password_confirmation"
+                  onChange={props.handleInputChange}
+                  onBlur={props.validateInput}
+                  error={props.isInputInvalid("password")}
+                  minLength={8}
+                  required
+                />
+
+                <Button
+                  color="teal"
+                  fluid
+                  size="large"
+                  onClick={props.onSubmitRegistration}
+                >
+                  Login
+                </Button>
+              </Segment>
+            </Form>
+            <Message>
+              Already have an account ? <Link to="/login">Sign In</Link>
+            </Message>
+          </Grid.Column>
+        </Grid>
+      </div>
+    </section>
   );
 };
 
--- src/scenes/registrations/containers/registrations_container.js
+++ src/scenes/registrations/containers/registrations_container.js
@@ -1,33 +1,86 @@
-import React, { Component } from 'react';
-import RegistrationsComponent from './../components/registrations_component'
-import * as registrations_actions from './../actions'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import React, { Component } from "react";
+import RegistrationsComponent from "./../components/registrations_component";
+import * as registrations_actions from "./../actions";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
 class RegistrationsContainer extends Component {
-
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      username: '',
-      email: '',
-      password: '',
-      password_confirmation: '',
+      username: "",
+      email: "",
+      password: "",
+      password_confirmation: "",
       errors: {}
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.isInputInvalid = this.isInputInvalid.bind(this);
+  }
+
+  validateEmailInput(target) {
+    const { name, value } = target;
+    if (name === "email" && value.length > 0 && value.includes("@") === false) {
+      return this.setState({
+        [`${name}-error`]: true
+      });
     }
+  }
+
+  validatePasswordInput(target) {
+    const { name, value } = target;
+    if (name === "password" && value.length < 8) {
+      return this.setState({
+        [`${name}-error`]: true
+      });
+    }
+  }
+
+  purgeErrorStates(name) {
+    const currentState = this.state;
+    delete currentState[`${name}-error`];
+    this.setState(currentState);
+  }
+
+  validateInput = event => {
+    const isValid = element => element.checkValidity();
+    const { target } = event;
+    const { name, value } = target;
+    if (!isValid(target)) {
+      //target.focus();
+      this.validateEmailInput(target);
+      this.validatePasswordInput(target);
+    }
+
+    this.purgeErrorStates(name);
+  };
+
+  isInputInvalid(name) {
+    return this.state[name + "-error"];
   }
 
   parseRegister = () => {
-    if(this.state.password !== this.state.password_confirmation){
-      this.setState({errors: {message: 'Password does not match'}})
+    if (this.state.password !== this.state.password_confirmation) {
+      this.setState({ errors: { message: "Password does not match" } });
+      return;
     }
-    this.props.postRegistration(this.state.username, this.state.email, this.state.password)
-  }
+    if (this.state.username.length < 4) {
+      this.setState({
+        errors: { message: "Username must be at least 4 characters long" }
+      });
+      return;
+    }
+    this.props.postRegistration(
+      this.state.username,
+      this.state.email,
+      this.state.password
+    );
+  };
 
-  handleInputChange = (event) => {
+  handleInputChange(event) {
     this.setState({
       [event.target.name]: event.target.value
-    })
+    });
   }
 
   render() {
@@ -40,23 +93,29 @@ class RegistrationsContainer extends Component {
           password={this.state.password}
           password_confirmation={this.state.password_confirmation}
           errors={this.props.errors}
-          onHandleInputChange={this.handleInputChange}
+          state_errors={this.state.errors}
+          handleInputChange={this.handleInputChange}
           onSubmitRegistration={this.parseRegister}
+          validationErrors={this.props.validationErrors}
+          validateInput={this.validateInput}
+          isInputInvalid={this.isInputInvalid}
         />
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     session: state.RegistrationsReducer.session,
     errors: state.RegistrationsReducer.errors
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators(registrations_actions, dispatch)
-}
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(registrations_actions, dispatch);
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(RegistrationsContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(
+  RegistrationsContainer
+);
