201414dd42f3bff5bcd0c5a6264a9eeb48920857
Olivier Ducroux <olivier@dmates.io>
2018-03-23 15:44:52 +0100

    Trying to solve styles issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "semantic-ui-css/semantic.min.css";
-import "./index.css";
+//import "./index.css";
 import App from "./main/app";
 import registerServiceWorker from "./registerServiceWorker";
 import Parse from "parse";
