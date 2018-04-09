4790b0d274a3d11b480227dddecf55dc2e231ccd
Olivier Ducroux <olivier@dmates.io>
2018-04-09 18:15:23 +0200

    Allowing services search filtering by location

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import registerServiceWorker from "./registerServiceWorker";
 import Parse from "parse";
 
 Parse.initialize("myAppId");
-Parse.serverURL =
-  process.env.REACT_APP_PARSE_SERVER_URL ||
-  "http://please-api.herokuapp.com/parse"; //"http://api.please.com/parse";
+Parse.serverURL = "http://api.please.docker/parse";
+// process.env.REACT_APP_PARSE_SERVER_URL ||
+// "http://please-api.herokuapp.com/parse"; //"http://api.please.com/parse";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
