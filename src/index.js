6f62fbae5fae494e0050268795223f5de7c689e8
Olivier Ducroux <olivier@dmates.io>
2018-04-20 16:16:04 +0200

    importing slick carousel css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "semantic-ui-css/semantic.min.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./index.css";
 import App from "./main/app";
 import registerServiceWorker from "./registerServiceWorker";
