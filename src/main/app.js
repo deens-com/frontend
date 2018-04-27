530215bc01d9d1a0510f694a995d1ed8547776f2
Olivier Ducroux <olivier@dmates.io>
2018-04-27 16:53:35 +0200

    removing react-ga

--- src/main/app.js
+++ src/main/app.js
@@ -4,8 +4,6 @@ import { Switch } from "react-router";
 import { Router, Route, Layout, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
-import ReactGA from "react-ga";
-import withTracker from "./middlewares/with_tracker";
 import Home from "./../scenes/home/home";
 import Sessions from "./../scenes/sessions/sessions";
 import Results from "./../scenes/results/results";
@@ -22,31 +20,31 @@ const App = () => {
           <Route
             exact
             path={process.env.PUBLIC_URL + "/"}
-            component={withTracker(Home)}
+            component={Home}
           />
           <Route
             path={process.env.PUBLIC_URL + "/login"}
-            component={withTracker(Sessions)}
+            component={Sessions}
           />
           <Route
             path={process.env.PUBLIC_URL + "/register"}
-            component={withTracker(Registrations)}
+            component={Registrations}
           />
           <Route
             path={process.env.PUBLIC_URL + "/results"}
-            component={withTracker(Results)}
+            component={Results}
           />
           <Route
             path={process.env.PUBLIC_URL + "/services/:id"}
-            component={withTracker(Services)}
+            component={Services}
           />
           <Route
             path={process.env.PUBLIC_URL + "/trips/:id"}
-            component={withTracker(Trips)}
+            component={Trips}
           />
           <Route
             path={process.env.PUBLIC_URL + "/users"}
-            component={withTracker(Users)}
+            component={Users}
           />
         </Switch>
       </HashRouter>
--- src/main/middlewares/with_tracker.js
+++ src/main/middlewares/with_tracker.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react'
-import GoogleAnalytics from 'react-ga'
-
-//GoogleAnalytics.initialize('UA-XXXXXX-2', { debug: true })
-GoogleAnalytics.initialize('UA-XXXXXXX-2')
-
-const withTracker = (WrappedComponent, options = {}) => {
-  const trackPage = page => {
-    GoogleAnalytics.set({
-      page,
-      ...options,
-    })
-    GoogleAnalytics.pageview(page)
-  }
-
-  const HOC = class extends Component {
-    componentDidMount() {
-      const page = this.props.location.pathname
-      trackPage(page)
-    }
-
-    componentWillReceiveProps(nextProps) {
-      const currentPage = this.props.location.pathname
-      const nextPage = nextProps.location.pathname
-
-      if (currentPage !== nextPage) {
-        trackPage(nextPage)
-      }
-    }
-
-    render() {
-      return <WrappedComponent {...this.props} />
-    }
-  }
-
-  return HOC
-}
-
-export default withTracker
+// import React, { Component } from 'react'
+// import GoogleAnalytics from 'react-ga'
+//
+// //GoogleAnalytics.initialize('UA-XXXXXX-2', { debug: true })
+// GoogleAnalytics.initialize('UA-XXXXXXX-2')
+//
+// const withTracker = (WrappedComponent, options = {}) => {
+//   const trackPage = page => {
+//     GoogleAnalytics.set({
+//       page,
+//       ...options,
+//     })
+//     GoogleAnalytics.pageview(page)
+//   }
+//
+//   const HOC = class extends Component {
+//     componentDidMount() {
+//       const page = this.props.location.pathname
+//       trackPage(page)
+//     }
+//
+//     componentWillReceiveProps(nextProps) {
+//       const currentPage = this.props.location.pathname
+//       const nextPage = nextProps.location.pathname
+//
+//       if (currentPage !== nextPage) {
+//         trackPage(nextPage)
+//       }
+//     }
+//
+//     render() {
+//       return <WrappedComponent {...this.props} />
+//     }
+//   }
+//
+//   return HOC
+// }
+//
+// export default withTracker
