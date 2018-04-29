4950108d6840f1434ceebacd91803db320892ab0
Jaydeep Solanki <jaydp17@gmail.com>
2018-04-29 22:53:07 +0530

    profile: remove title

--- src/styled_scenes/Users/components/StatusAndBio.js
+++ src/styled_scenes/Users/components/StatusAndBio.js
@@ -3,12 +3,6 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { SectionWrap } from '../../../shared_components/layout/Page';
 
-const Title = styled.p`
-  font-size: 36px;
-  font-weight: 700;
-  margin-bottom: 12px;
-`;
-
 const SubTitle = styled.p`
   color: #a3a9b2;
 `;
@@ -16,7 +10,6 @@ const SubTitle = styled.p`
 const StatusAndBio = ({ user = {} }) => {
   return (
     <SectionWrap>
-      {user.profileTitle && <Title>{user.profileTitle}</Title>}
       {user.bio && <SubTitle>{user.bio}</SubTitle>}
     </SectionWrap>
   );
