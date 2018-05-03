fddd253ffe62542db32709be5bbf7602cd09bfe4
Olivier Ducroux <olivier@dmates.io>
2018-05-03 09:51:59 +0200

    created TopBarWithSearch shared component

---
+++ src/shared_components/TopBarWithSearch/Search/index.js
@@ -0,0 +1,48 @@
+// NPM
+import React from "react";
+import PropTypes from "prop-types";
+import Media from "react-media";
+import styled from "styled-components";
+
+// COMPONENTS
+import { SearchIcon } from "../../../shared_components/icons";
+import DesktopSearch from "./DesktopSearch";
+
+// ACTIONS/CONFIG
+import { sizes, resetButton } from "../../../libs/styled";
+
+// STYLES
+const TriggerWrap = styled.button`
+  ${resetButton()};
+  height: 50px;
+  margin-left: auto;
+  padding: 10px;
+  width: 50px;
+  color: #a2b2c0;
+  font-size: 24px;
+`;
+
+// MODULE
+export default function TopBarSearch({ menuIsOpened, toggleSearch }) {
+  if (menuIsOpened) return null;
+
+  return (
+    <Media query={`(max-width: ${sizes.small})`}>
+      {matches =>
+        matches ? (
+          <TriggerWrap onClick={toggleSearch}>
+            <SearchIcon />
+          </TriggerWrap>
+        ) : (
+          <DesktopSearch toggleSearch={toggleSearch} />
+        )
+      }
+    </Media>
+  );
+}
+
+// Props Validation
+TopBarSearch.propTypes = {
+  menuIsOpened: PropTypes.bool.isRequired,
+  toggleSearch: PropTypes.func.isRequired
+};
