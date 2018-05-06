57752b02827f7c0b2dc1ac33feba36866a693c35
Jaydeep Solanki <jaydp17@gmail.com>
2018-05-06 15:15:39 +0530

    add UserAvatar component

---
+++ src/shared_components/UserAvatar/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Grid, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import Rating from '../../shared_components/Rating';
+
+const AvatarWrapper = styled.div`
+  height: 30px;
+  width: 30px;
+`;
+
+const TextWrapper = styled.div`
+  padding-left: 18px;
+`;
+
+const WhiteText = styled.span`
+  color: #fff;
+`;
+
+const Avatar = ({ user }) => {
+  if (!user) return null;
+  const dpUrl = (user.profilePicture && user.profilePicture.url) || 'https://imgur.com/download/4iTD3lS';
+  const userProfilePageUrl = `users/${user.username}`;
+  return (
+    <Grid columns={2} padded="horizontally" verticalAlign="middle">
+      <Grid.Row>
+        <Grid.Column width={3}>
+          <Link to={userProfilePageUrl}>
+            <AvatarWrapper>
+              <Image src={dpUrl} circular />
+            </AvatarWrapper>
+          </Link>
+        </Grid.Column>
+        <Grid.Column width={13}>
+          <TextWrapper>
+            <Link to={userProfilePageUrl}>
+              <WhiteText>{user.username}</WhiteText>
+            </Link>
+            <Rating rating={4} />
+          </TextWrapper>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
+};
+
+Avatar.propTypes = {
+  user: PropTypes.object,
+};
+
+Avatar.defaultProps = {
+  user: null,
+};
+
+export default Avatar;
