d9cf10387427232cf7b139c72691d36e00cc726c
Vladimir Kokovic <vlada.kokovic@gmail.com>
2018-05-21 16:44:53 +0200

    add pagination, display 12 results per page

--- src/styled_scenes/Results/components/Results.js
+++ src/styled_scenes/Results/components/Results.js
@@ -1,11 +1,12 @@
 // NPM
-import React from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 // COMPONENTS
 import Row from "../../../shared_components/layout/Row";
 import TripCart from "../../../shared_components/Carts/Trip";
+import ReactPaginate from 'react-paginate';
 
 // STYLES
 const Wrap = styled.div`
@@ -20,28 +21,111 @@ const Header = styled.div`
   }
 `;
 
-// MODULE
-export default function Results(props) {
-  return (
-    <Wrap>
-      <Header>
-        <h4>{props.service_type} for you</h4>
-      </Header>
-      <Row>
-        {props.data.map(result => (
-          <Link to={(result.type ? "/services/" : "/trips/") + result.objectId} key={result.objectId}>
-            <TripCart
-              key={result.label}
-              withTooltip
-              withShadow
-              item={result}
-            />
-          </Link>
-        ))}
-      </Row>
-    </Wrap>
-  );
-}
+const PaginationWrap = styled.div`
+  text-align: center;
 
-// Props Validation
-Results.propTypes = {};
+  ul {
+    display: inline-block;
+    list-style-type: none;
+
+    li {
+      float: left;
+      padding: 5px 10px;
+      margin: 0 5px;
+      border: 1px solid grey;
+      border-radius: 4px;
+      cursor: pointer;
+
+      a {
+        &:focus {
+          outline: none;
+        }
+      }
+
+      &.selected {
+        border: 1px solid #4fb798;
+        a {
+          color: #4fb798;
+        }
+      }
+
+      &.disabled {
+        visibility: hidden;
+      }
+    }
+  }
+`;
+
+
+const limit_per_page = 12;
+
+// MODULE
+export default class Results extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filteredData: [],
+      totalItems: 0
+    }
+  }
+
+  componentWillReceiveProps() {
+    this.loadData();
+    this.setState({totalItems: this.props.data.length});
+  }
+
+  loadData(item) {
+    let data = [];
+    let skip = 0;
+    let selected = 1;
+
+    if (item) {
+      selected = item.selected;
+    }
+
+    this.props.data.forEach((result, i) => {
+      if (i < (selected * limit_per_page)) {
+        skip++
+        return;
+      }
+
+      if (i < (skip + limit_per_page)) {
+        data.push(result);
+      }
+    });
+
+    this.setState({filteredData: data});
+  }
+
+  render() {
+    return (
+      <Wrap>
+        <Header>
+          <h4>{this.props.service_type} for you</h4>
+        </Header>
+        <Row>
+          {this.state.filteredData.map((result, i) => (
+            <Link to={(result.type ? "/services/" : "/trips/") + result.objectId} key={result.objectId}>
+              <TripCart
+                key={result.label}
+                withTooltip
+                withShadow
+                item={result}
+              />
+            </Link>
+          ))}
+        </Row>
+        <Row>
+          <PaginationWrap>
+            <ReactPaginate pageCount={this.state.totalItems / limit_per_page}
+                           marginPagesDisplayed={2}
+                           pageRangeDisplayed={5}
+                           onPageChange={this.loadData.bind(this)} />
+           </PaginationWrap>
+        </Row>
+      </Wrap>
+    );
+  }
+}
