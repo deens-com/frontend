5f2db460c4646f0073268dbd4eeec12a83cab5a4
Olivier Ducroux <olivier@dmates.io>
2018-05-24 15:54:41 +0200

    Show Start Date and Day Count Of Trips In Service Adding Page

--- src/styled_scenes/Service/components/AddToTripButton/TripsListInDropDown.js
+++ src/styled_scenes/Service/components/AddToTripButton/TripsListInDropDown.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Image, List } from 'semantic-ui-react';
+import moment from "moment";
 
 const getTripImage = trip => {
   if (trip.picture && trip.picture.url) return trip.picture.url;
@@ -8,9 +9,26 @@ const getTripImage = trip => {
 };
 
 class TripsListInDropDown extends React.Component {
+
   onItemClick = trip => () => {
     this.props.onTripClick(trip);
   };
+
+  getStartTripDate = (trip) => {
+    if(trip.beginDate){
+      return "- " + moment(trip.beginDate).format("DD-MM-YYYY") + ", ";
+    }else{
+      return undefined;
+    }
+  }
+
+  getTripDuration = (trip) => {
+    if(trip.beginDate && trip.endDate){
+      return moment.duration(moment(trip.endDate).diff(moment(trip.beginDate))).asDays() + " Days";
+    }else{
+      return undefined;
+    }
+  }
 
   render() {
     return (
@@ -19,7 +37,10 @@ class TripsListInDropDown extends React.Component {
           <List.Item key={trip.objectId} onClick={this.onItemClick(trip)}>
             <Image avatar src={getTripImage(trip)} />
             <List.Content>
-              <List.Header>{trip.title}</List.Header>
+              <List.Header>
+                {trip.title} {this.getStartTripDate(trip)}
+                {this.getTripDuration(trip)}
+              </List.Header>
             </List.Content>
           </List.Item>
         ))}
