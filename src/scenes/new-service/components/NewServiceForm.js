40ce95043eb8bbd3a9325f4b2fb7c1e5e3ce7711
Jaydeep Solanki <jaydp17@gmail.com>
2018-05-27 15:57:42 +0530

    add handlers for dropdowns

--- src/scenes/new-service/components/NewServiceForm.js
+++ src/scenes/new-service/components/NewServiceForm.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Dropdown, Input, Form } from 'semantic-ui-react';
 import { withFormik } from 'formik';
 import styled from 'styled-components';
-import { Creatable } from 'react-select';
 
 const serviceTypes = ['Place', 'Activity', 'Food'];
 const serviceTypeDropdownOptions = serviceTypes.map(text => ({ value: text.toLowerCase(), text }));
@@ -14,101 +13,148 @@ const ErrorMsg = styled.div`
   color: red;
 `;
 
-const NewServiceForm = ({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => {
-  const defaultProps = {
-    onChange: handleChange,
-    onBlur: handleBlur,
+class NewServiceForm extends Component {
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
   };
-  return (
-    <Form onSubmit={handleSubmit}>
-      {/* Type */}
-      <Form.Field required>
-        <label>Service type</label>
-        <Dropdown placeholder="Service Type" selection options={serviceTypeDropdownOptions} />
-      </Form.Field>
 
-      {/* Name */}
-      <Form.Field required>
-        <label>Service name</label>
-        <Form.Input name="name" placeholder="Service name" error={touched.name && errors.name} {...defaultProps} />
-        {touched.name && errors.name && <ErrorMsg>{errors.name}</ErrorMsg>}
-      </Form.Field>
+  state = {
+    tagOptions: [],
+  };
 
-      {/* Description */}
-      <Form.Field required>
-        <label>Service description</label>
-        <Form.TextArea
-          name="description"
-          placeholder="Tell us more..."
-          error={touched.description && errors.description}
-          {...defaultProps}
-        />
-        {touched.description && errors.description && <ErrorMsg>{errors.description}</ErrorMsg>}
-      </Form.Field>
+  componentDidMount() {
+    this.props.setFieldValue('tags', []);
+  }
 
-      {/* Price */}
-      <Form.Field required>
-        <label>Price Per Day/Night</label>
-        <Form.Input name="price" type="number" error={touched.price && errors.price} {...defaultProps} />
-        {touched.price && errors.price && <ErrorMsg>{errors.price}</ErrorMsg>}
-      </Form.Field>
+  onTagAddition = (e, { value }) => {
+    this.setState(prevState => ({
+      tagOptions: [{ text: value, value }, ...prevState.tagOptions],
+    }));
+  };
 
-      {/* Accept Ethereum */}
-      <Form.Field>
-        <label>Accept Ethereum</label>
-        <Form.Checkbox id="acceptEthereum" name="acceptEthereum" {...defaultProps} />
-      </Form.Field>
+  onDropDownChange = (e, { name, value }) => {
+    const { setFieldValue } = this.props;
+    setFieldValue(name, value);
+  };
 
-      {/* Available Days */}
-      <Form.Group grouped>
-        <label>Available Days</label>
-        <Form.Checkbox id="sunday" name="sunday" label="Sunday" {...defaultProps} />
-        <Form.Checkbox id="monday" name="monday" label="Monday" {...defaultProps} />
-        <Form.Checkbox id="tuesday" name="tuesday" label="Tuesday" {...defaultProps} />
-        <Form.Checkbox id="wednesday" name="wednesday" label="Wednesday" {...defaultProps} />
-        <Form.Checkbox id="thursday" name="thursday" label="Thursday" {...defaultProps} />
-        <Form.Checkbox id="friday" name="friday" label="Friday" {...defaultProps} />
-        <Form.Checkbox id="saturday" name="saturday" label="Saturday" {...defaultProps} />
-      </Form.Group>
+  render() {
+    const { values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting } = this.props;
+    const defaultProps = {
+      onChange: handleChange,
+      onBlur: handleBlur,
+    };
+    return (
+      <Form onSubmit={handleSubmit}>
+        {/* Service Type */}
+        <Form.Field required>
+          <label>Service type</label>
+          <Dropdown
+            name="serviceType"
+            placeholder="Service Type"
+            selection
+            options={serviceTypeDropdownOptions}
+            onChange={this.onDropDownChange}
+          />
+        </Form.Field>
 
-      {/* Lat/Lng */}
-      <Form.Group widths="equal">
-        <Form.Input name="latitude" label="Latitude" {...defaultProps} />
-        <Form.Input name="longitude" label="Longitude" {...defaultProps} />
-      </Form.Group>
+        {/* Name */}
+        <Form.Field required>
+          <label>Service name</label>
+          <Form.Input name="name" placeholder="Service name" error={touched.name && errors.name} {...defaultProps} />
+          {touched.name && errors.name && <ErrorMsg>{errors.name}</ErrorMsg>}
+        </Form.Field>
 
-      {/* Timings */}
-      <Form.Group widths="equal">
-        <Form.Dropdown
-          name="openingTime"
-          label="Opening time"
-          placeholder="Select opening time"
-          selection
-          options={hoursDropdownOptions}
-        />
-        <Form.Dropdown
-          name="closingTime"
-          label="Closing time"
-          placeholder="Select closing time"
-          selection
-          options={hoursDropdownOptions}
-        />
-      </Form.Group>
+        {/* Description */}
+        <Form.Field required>
+          <label>Service description</label>
+          <Form.TextArea
+            name="description"
+            placeholder="Tell us more..."
+            error={touched.description && errors.description}
+            {...defaultProps}
+          />
+          {touched.description && errors.description && <ErrorMsg>{errors.description}</ErrorMsg>}
+        </Form.Field>
 
-      {/* Tags */}
-      <Form.Field>
-        <label>Tags</label>
-        <Creatable name="tags" placeholder="Select..." {...defaultProps} inputProps={{ type: 'react-type' }} />
-      </Form.Field>
+        {/* Price */}
+        <Form.Field required>
+          <label>Price Per Day/Night</label>
+          <Form.Input name="price" type="number" error={touched.price && errors.price} {...defaultProps} />
+          {touched.price && errors.price && <ErrorMsg>{errors.price}</ErrorMsg>}
+        </Form.Field>
 
-      <Form.Button disabled={isSubmitting}>Submit</Form.Button>
-    </Form>
-  );
-};
+        {/* Accept Ethereum */}
+        <Form.Field>
+          <label>Accept Ethereum</label>
+          <Form.Checkbox id="acceptEthereum" name="acceptEthereum" {...defaultProps} />
+        </Form.Field>
 
-NewServiceForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-};
+        {/* Available Days */}
+        <Form.Group grouped>
+          <label>Available Days</label>
+          <Form.Checkbox id="sunday" name="sunday" label="Sunday" {...defaultProps} />
+          <Form.Checkbox id="monday" name="monday" label="Monday" {...defaultProps} />
+          <Form.Checkbox id="tuesday" name="tuesday" label="Tuesday" {...defaultProps} />
+          <Form.Checkbox id="wednesday" name="wednesday" label="Wednesday" {...defaultProps} />
+          <Form.Checkbox id="thursday" name="thursday" label="Thursday" {...defaultProps} />
+          <Form.Checkbox id="friday" name="friday" label="Friday" {...defaultProps} />
+          <Form.Checkbox id="saturday" name="saturday" label="Saturday" {...defaultProps} />
+        </Form.Group>
+
+        {/* Lat/Lng */}
+        <Form.Group widths="equal">
+          <Form.Input name="latitude" label="Latitude" {...defaultProps} />
+          <Form.Input name="longitude" label="Longitude" {...defaultProps} />
+        </Form.Group>
+
+        {/* Timings */}
+        <Form.Group widths="equal">
+          <Form.Dropdown
+            name="openingTime"
+            label="Opening time"
+            placeholder="Select opening time"
+            selection
+            options={hoursDropdownOptions}
+            onChange={this.onDropDownChange}
+          />
+          <Form.Dropdown
+            name="closingTime"
+            label="Closing time"
+            placeholder="Select closing time"
+            selection
+            options={hoursDropdownOptions}
+            onChange={this.onDropDownChange}
+          />
+        </Form.Group>
+
+        {/* Tags */}
+        <Form.Field>
+          <label>Tags</label>
+          <Dropdown
+            name="tags"
+            options={this.state.tagOptions}
+            placeholder="Add tags"
+            search
+            selection
+            fluid
+            multiple
+            allowAdditions
+            value={values.tags}
+            onAddItem={this.onTagAddition}
+            onChange={this.onDropDownChange}
+          />
+        </Form.Field>
+
+        <Form.Field>
+          <label>Service Picture</label>
+          <Input type="file" name="picture" />
+        </Form.Field>
+
+        <Form.Button disabled={isSubmitting}>Submit</Form.Button>
+      </Form>
+    );
+  }
+}
 
 export default withFormik({
   handleSubmit: (values, { props }) => {
