e1a9f44c09cbd3d7867bf4d35abfdb36012bce6d
Jaydeep Solanki <jaydp17@gmail.com>
2018-05-27 17:44:33 +0530

    validate required fields and show errors

--- src/scenes/new-service/components/NewServiceForm.js
+++ src/scenes/new-service/components/NewServiceForm.js
@@ -8,6 +8,7 @@ const serviceTypes = ['Place', 'Activity', 'Food'];
 const serviceTypeDropdownOptions = serviceTypes.map(text => ({ value: text.toLowerCase(), text }));
 const hours = Array.from({ length: 24 }, (v, k) => k);
 const hoursDropdownOptions = hours.map(h => ({ value: h, text: h.toString().padStart(2, '0') + ':00' }));
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 const ErrorMsg = styled.div`
   color: red;
@@ -22,10 +23,6 @@ class NewServiceForm extends Component {
     tagOptions: [],
   };
 
-  componentDidMount() {
-    this.props.setFieldValue('tags', []);
-  }
-
   onTagAddition = (e, { value }) => {
     this.setState(prevState => ({
       tagOptions: [{ text: value, value }, ...prevState.tagOptions],
@@ -33,8 +30,23 @@ class NewServiceForm extends Component {
   };
 
   onDropDownChange = (e, { name, value }) => {
-    const { setFieldValue } = this.props;
+    const { setFieldValue, setFieldTouched } = this.props;
     setFieldValue(name, value);
+    setFieldTouched(name, true, false);
+  };
+
+  onAvailableDaysChange = (e, { label, checked }) => {
+    const { values, setFieldValue, setFieldTouched } = this.props;
+    const availableDays = values.availableDays;
+    if (checked) {
+      // checked
+      availableDays.add(label);
+    } else {
+      // unchecked
+      availableDays.delete(label);
+    }
+    setFieldValue('availableDays', availableDays);
+    setFieldTouched('availableDays', true, false);
   };
 
   render() {
@@ -55,12 +67,18 @@ class NewServiceForm extends Component {
             options={serviceTypeDropdownOptions}
             onChange={this.onDropDownChange}
           />
+          {touched.serviceType && errors.serviceType && <ErrorMsg>{errors.serviceType}</ErrorMsg>}
         </Form.Field>
 
         {/* Name */}
         <Form.Field required>
           <label>Service name</label>
-          <Form.Input name="name" placeholder="Service name" error={touched.name && errors.name} {...defaultProps} />
+          <Form.Input
+            name="name"
+            placeholder="Service name"
+            error={!!(touched.name && errors.name)}
+            {...defaultProps}
+          />
           {touched.name && errors.name && <ErrorMsg>{errors.name}</ErrorMsg>}
         </Form.Field>
 
@@ -70,7 +88,7 @@ class NewServiceForm extends Component {
           <Form.TextArea
             name="description"
             placeholder="Tell us more..."
-            error={touched.description && errors.description}
+            error={!!(touched.description && errors.description)}
             {...defaultProps}
           />
           {touched.description && errors.description && <ErrorMsg>{errors.description}</ErrorMsg>}
@@ -79,7 +97,7 @@ class NewServiceForm extends Component {
         {/* Price */}
         <Form.Field required>
           <label>Price Per Day/Night</label>
-          <Form.Input name="price" type="number" error={touched.price && errors.price} {...defaultProps} />
+          <Form.Input name="price" type="number" error={!!(touched.price && errors.price)} {...defaultProps} />
           {touched.price && errors.price && <ErrorMsg>{errors.price}</ErrorMsg>}
         </Form.Field>
 
@@ -92,13 +110,17 @@ class NewServiceForm extends Component {
         {/* Available Days */}
         <Form.Group grouped>
           <label>Available Days</label>
-          <Form.Checkbox id="sunday" name="sunday" label="Sunday" {...defaultProps} />
-          <Form.Checkbox id="monday" name="monday" label="Monday" {...defaultProps} />
-          <Form.Checkbox id="tuesday" name="tuesday" label="Tuesday" {...defaultProps} />
-          <Form.Checkbox id="wednesday" name="wednesday" label="Wednesday" {...defaultProps} />
-          <Form.Checkbox id="thursday" name="thursday" label="Thursday" {...defaultProps} />
-          <Form.Checkbox id="friday" name="friday" label="Friday" {...defaultProps} />
-          <Form.Checkbox id="saturday" name="saturday" label="Saturday" {...defaultProps} />
+          {weekDays.map(weekDay => (
+            <Form.Checkbox
+              id={weekDay}
+              label={weekDay}
+              key={weekDay}
+              name={weekDay}
+              checked={values.availableDays.has(weekDay)}
+              onChange={this.onAvailableDaysChange}
+            />
+          ))}
+          {touched.availableDays && errors.availableDays && <ErrorMsg>{errors.availableDays}</ErrorMsg>}
         </Form.Group>
 
         {/* Lat/Lng */}
@@ -156,7 +178,27 @@ class NewServiceForm extends Component {
   }
 }
 
+function validate(values) {
+  const requiredFields = ['serviceType', 'name', 'description', 'price', 'availableDays'];
+  const errors = checkRequiredFields(values, requiredFields);
+  // errors.serviceType = validateServiceType(values.serviceType);
+  return errors;
+}
+
+function checkRequiredFields(values, requiredFields) {
+  return requiredFields.reduce((errors, fieldName) => {
+    const fieldValue = values[fieldName];
+    if (!fieldValue || fieldValue.length === 0 || fieldValue.size === 0) errors[fieldName] = 'Required';
+    return errors;
+  }, {});
+}
+
 export default withFormik({
+  mapPropsToValues: () => ({
+    tags: [],
+    availableDays: new Set(),
+  }),
+  validate,
   handleSubmit: (values, { props }) => {
     props.onSubmit(values);
   },
