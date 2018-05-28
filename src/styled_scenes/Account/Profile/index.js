555c22be2ac522973404ac14b8a2419ee68976ad
Olivier Ducroux <olivier@dmates.io>
2018-05-28 11:33:02 +0200

    displaying basic user infos

--- src/styled_scenes/Account/Profile/index.js
+++ src/styled_scenes/Account/Profile/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Divider, Icon } from 'semantic-ui-react';
+import styled from "styled-components";
 import { SectionWrap } from './../../../shared_components/layout/Page';
 import UserBasicInfo from './../components/UserBasicInfo';
+
+const HorizontalSpan = styled.span`
+  display: flex;
+  flexDirection: row;
+`;
+
+const BoldH4 = styled.h4`
+  font-weight: bold;
+`;
 
 const AccountProfileScene = props => {
   return (
@@ -14,6 +24,19 @@ const AccountProfileScene = props => {
       </Grid.Column>
       <Grid.Column mobile={16} tablet={11} computer={12}>
         <h2>Profile Scene</h2>
+        <Divider />
+        <HorizontalSpan>
+          <BoldH4>Bio :</BoldH4>
+          <p>&nbsp;{props.user_profile.biography}</p>&nbsp;<Icon disabled name='pencil' />
+        </HorizontalSpan>
+        <HorizontalSpan>
+          <BoldH4>Username :</BoldH4>
+          <p>&nbsp;{props.user_profile.username}</p>&nbsp;<Icon disabled name='pencil' />
+        </HorizontalSpan>
+        <HorizontalSpan>
+          <BoldH4>Email :</BoldH4>
+          <p>&nbsp;{props.user_profile.email}</p>&nbsp;<Icon disabled name='pencil' />
+        </HorizontalSpan>
       </Grid.Column>
     </Grid>
   );
