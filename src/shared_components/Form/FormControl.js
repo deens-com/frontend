aa940a3761e82654561924af72245eef1c8fde99
Olivier Ducroux <olivier@dmates.io>
2018-06-01 11:49:30 +0200

    fix

--- src/shared_components/Form/FormControl.js
+++ src/shared_components/Form/FormControl.js
@@ -165,7 +165,7 @@ export default class FormControl extends Component {
             focused={this.state.focused}
             onFocus={this.onFocus}
             onBlur={this.onBlur}
-            value={this.props.value}
+            value={this.state.value}
           />
         );
         break;
