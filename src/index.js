d818aef648363de4f10876669e825c38a59c3b97
Vladimir Kokovic <vlada.kokovic@gmail.com>
2018-06-06 15:01:06 +0200

    add sentry through raven module

--- src/index.js
+++ src/index.js
@@ -7,11 +7,15 @@ import "./index.css";
 import App from "./main/app";
 import registerServiceWorker from "./registerServiceWorker";
 import Parse from "parse";
+import Raven from 'raven-js';
 
 Parse.initialize("myAppId");
-Parse.serverURL = //"https://api.please.docker/parse";
-  process.env.REACT_APP_PARSE_SERVER_URL ||
-  "https://please-api.herokuapp.com/parse";
+Parse.serverURL = "https://api.please.docker/parse";
+
+if(process.env.NODE_ENV === "production"){
+  Raven.config("https://fd51482cf40f43fca379bc14417b6f2b@sentry.io/1220761").install();
+  Parse.serverURL = process.env.REACT_APP_PARSE_SERVER_URL || "https://please-api.herokuapp.com/parse";
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
