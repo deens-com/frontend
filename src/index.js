973be32d1d3f62b6e1923c8505a16911e8cbfe01
Jaydeep Solanki <jaydp17@gmail.com>
2018-06-12 08:58:29 +0530

    un-register service worker

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "semantic-ui-css/semantic.min.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "./index.css";
-import App from "./main/app";
-import registerServiceWorker from "./registerServiceWorker";
-import Parse from "parse";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './index.css';
+import App from './main/app';
+import { unregister as unregisterServiceWorker } from './registerServiceWorker';
+import Parse from 'parse';
 import Raven from 'raven-js';
 
-Parse.initialize("myAppId");
-Parse.serverURL = "https://api.please.docker/parse";
+Parse.initialize('myAppId');
+Parse.serverURL = 'https://api.please.docker/parse';
 
-if(process.env.NODE_ENV === "production"){
-  Raven.config("https://fd51482cf40f43fca379bc14417b6f2b@sentry.io/1220761").install();
-  Parse.serverURL = process.env.REACT_APP_PARSE_SERVER_URL || "https://please-api.herokuapp.com/parse";
+if (process.env.NODE_ENV === 'production') {
+  Raven.config('https://fd51482cf40f43fca379bc14417b6f2b@sentry.io/1220761').install();
+  Parse.serverURL = process.env.REACT_APP_PARSE_SERVER_URL || 'https://please-api.herokuapp.com/parse';
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
+unregisterServiceWorker();
