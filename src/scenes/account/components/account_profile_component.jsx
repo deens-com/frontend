2ae0bc8a60fe6f6fad74f04ca15a18c5c88ae27d
Vladimir Kokovic <vlada.kokovic@gmail.com>
2018-06-14 15:12:45 +0200

    fetch user profile

--- src/scenes/account/components/account_profile_component.jsx
+++ src/scenes/account/components/account_profile_component.jsx
@@ -3,9 +3,20 @@ import PropTypes from 'prop-types';
 import AccountProfileScene from './../../../styled_scenes/Account/Profile';
 import { Page, PageContent } from './../../../shared_components/layout/Page';
 import TopBar from '../../../shared_components/TopBarWithSearch';
+import * as account_actions from "./../actions";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
 class AccountProfileComponent extends Component {
-  
+
+  static propTypes = {
+    user_profile: PropTypes.object
+  };
+
+  componentDidMount(){
+    this.props.fetch_user_profile();
+  }
+
   render(){
     return (
       <section>
@@ -20,8 +31,14 @@ class AccountProfileComponent extends Component {
   }
 };
 
-AccountProfileComponent.propTypes = {
-  user: PropTypes.object,
+const mapStateToProps = state => {
+  return {
+    user_profile: state.AccountReducer.user_profile
+  };
 };
 
-export default AccountProfileComponent;
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(account_actions, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AccountProfileComponent);
