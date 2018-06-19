dc37b85be4eab1cc74b0ea2890aa382056892b9c
Jaydeep Solanki <jaydp17@gmail.com>
2018-06-19 09:21:29 +0530

    show number instead of Nb

--- src/styled_scenes/Results/components/SearchFilters.js
+++ src/styled_scenes/Results/components/SearchFilters.js
@@ -313,7 +313,7 @@ class SearchFilters extends Component {
                     <FormControl
                       type="number"
                       onChange={this.handlePersonChange}
-                      placeholder="Nb"
+                      placeholder="1"
                       leftIcon="person"
                       min={1}
                       max={10}
@@ -386,7 +386,7 @@ class SearchFilters extends Component {
           <FormControl
             type="number"
             onChange={this.handlePersonChange}
-            placeholder="Nb"
+            placeholder="1"
             leftIcon="person"
             min={1}
             max={10}
