b5299b0483d85db505e064d0d47313a1d77a5bb3
Jaydeep Solanki <jaydp17@gmail.com>
2018-06-22 15:51:57 +0530

    format

--- src/styled_scenes/Results/index.js
+++ src/styled_scenes/Results/index.js
@@ -1,23 +1,23 @@
 // NPM
-import React from "react";
-import Media from "react-media";
-import styled from "styled-components";
-import GoogleMapReact from "google-map-react";
+import React from 'react';
+import Media from 'react-media';
+import styled from 'styled-components';
+import GoogleMapReact from 'google-map-react';
 
 // COMPONENTS
-import TopBar from "./../../shared_components/TopBarWithSearch";
+import TopBar from './../../shared_components/TopBarWithSearch';
 
-import BrandFooter from "./../../shared_components/BrandFooter";
-import SearchFilters from "./components/SearchFilters";
-import Results from "./components/Results";
-import MapMaker from "./../../shared_components/MapMarker";
+import BrandFooter from './../../shared_components/BrandFooter';
+import SearchFilters from './components/SearchFilters';
+import Results from './components/Results';
+import MapMaker from './../../shared_components/MapMarker';
 
 // ACTIONS/CONFIG
-import { media, sizes } from "../../libs/styled";
+import { media, sizes } from '../../libs/styled';
 // import { foodList } from "../../data/food";
 
 // STYLES
-import { Page, PageContent } from "./../../shared_components/layout/Page";
+import { Page, PageContent } from './../../shared_components/layout/Page';
 
 const MapWrapper = styled.div`
   width: 42%;
@@ -45,14 +45,14 @@ const ServicesWrapper = styled.div`
 // MODULE
 export default function ResultsScene(props) {
   const geo = !props.service_data.length
-    ? {lat: (parseFloat(props.latitude) || 48.856614), lon: (parseFloat(props.longitude) || 2.3522219000000177)}
-    : {lat: props.service_data[0].latitude, lon: props.service_data[0].longitude}
+    ? { lat: parseFloat(props.latitude) || 48.856614, lon: parseFloat(props.longitude) || 2.3522219000000177 }
+    : { lat: props.service_data[0].latitude, lon: props.service_data[0].longitude };
   return (
     <Page topPush>
       <TopBar {...props} fixed withPadding />
       <PageContent flex>
         <ServicesWrapper>
-          <br/>
+          <br />
           <SearchFilters {...props} />
           <Results {...props} data={props.service_data} />
         </ServicesWrapper>
@@ -64,14 +64,18 @@ export default function ResultsScene(props) {
                 center={{ lat: geo.lat, lng: geo.lon }}
                 defaultZoom={12}
                 bootstrapURLKeys={{
-                  key: "AIzaSyDICUW2RF412bnmELi3Y_zCCzHa-w8WnXc"
+                  key: 'AIzaSyDICUW2RF412bnmELi3Y_zCCzHa-w8WnXc',
                 }}
               >
-                {
-                  props.service_data.map(service =>
-                    <MapMaker key={service.objectId} lat={parseFloat(service.latitude)} lng={parseFloat(service.longitude)} scale={1} color="#4fb798" />
-                  )
-                }
+                {props.service_data.map(service => (
+                  <MapMaker
+                    key={service.objectId}
+                    lat={parseFloat(service.latitude)}
+                    lng={parseFloat(service.longitude)}
+                    scale={1}
+                    color="#4fb798"
+                  />
+                ))}
               </GoogleMapReact>
             </MapWrapper>
           )}
