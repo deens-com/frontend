366b21e77be703925f0b610b2075c6bc7c4c100b
Jaydeep Solanki <jaydp17@gmail.com>
2018-06-28 13:25:22 +0530

    format

--- src/scenes/services/actions.js
+++ src/scenes/services/actions.js
@@ -1,55 +1,57 @@
-import Parse from "parse";
-import fetch_helpers from "./../../libs/fetch_helpers";
+import Parse from 'parse';
+import fetch_helpers from './../../libs/fetch_helpers';
 
 export const trips_fetched = trips => {
   return {
-    type: "TRIPS_FETCHED",
-    payload: trips
+    type: 'TRIPS_FETCHED',
+    payload: trips,
   };
 };
 
 export const reviews_fetched = reviews => {
   return {
-    type: "REVIEWS_FETCHED",
-    payload: reviews
+    type: 'REVIEWS_FETCHED',
+    payload: reviews,
   };
 };
 
 export const service_fetched = service => {
   return {
-    type: "SERVICE_FETCHED",
-    payload: service
+    type: 'SERVICE_FETCHED',
+    payload: service,
   };
 };
 
 export const set_service_unavailability_modal = bool => {
   return {
-    type: "SERVICE_UNAVAILABILITY_MODAL_SET",
-    payload: bool
+    type: 'SERVICE_UNAVAILABILITY_MODAL_SET',
+    payload: bool,
   };
 };
 
 export const userUnpurchasedTripsFetchStart = () => ({ type: 'USER_UNPURCHASED_TRIPS_FETCH' });
-export const userUnpurchasedTripsFetchFinish = trips => ({ type: 'USER_UNPURCHASED_TRIPS_FETCH_FINISH', payload: trips });
+export const userUnpurchasedTripsFetchFinish = trips => ({
+  type: 'USER_UNPURCHASED_TRIPS_FETCH_FINISH',
+  payload: trips,
+});
 
-export const fetch_service = (service_id) => {
+export const fetch_service = service_id => {
   return dispatch => {
-    let query = fetch_helpers.build_query("Service");
-    query.equalTo("objectId", service_id);
+    let query = fetch_helpers.build_query('Service');
+    query.equalTo('objectId', service_id);
     query.include('owner');
     query.find().then(
       response => {
-
         // Associated Trips
-        let trip_org_query = fetch_helpers.build_query("TripOrganization");
-        trip_org_query.include("trip");
-        trip_org_query.equalTo("service", response[0]);
+        let trip_org_query = fetch_helpers.build_query('TripOrganization');
+        trip_org_query.include('trip');
+        trip_org_query.equalTo('service', response[0]);
         trip_org_query.find().then(
           response => {
             const trips_organization = fetch_helpers.normalizeParseResponseData(response);
             const trips = trips_organization.map(trip_org => {
               return trip_org.trip;
-            })
+            });
             const serialized_trips = fetch_helpers.mapServiceObjects(trips);
             dispatch(trips_fetched({ trips: serialized_trips }));
           },
@@ -60,16 +62,16 @@ export const fetch_service = (service_id) => {
         );
 
         // Associated Reviews
-        let reviews_query = fetch_helpers.build_query("Review");
-        reviews_query.include("reviewer");
-        reviews_query.equalTo("service", response[0]);
+        let reviews_query = fetch_helpers.build_query('Review');
+        reviews_query.include('reviewer');
+        reviews_query.equalTo('service', response[0]);
         reviews_query.find().then(
           response => {
             const reviews = fetch_helpers.normalizeParseResponseData(response);
             const serialized_reviews = reviews.map(review => {
-              delete(review.service);
+              delete review.service;
               return review;
-            })
+            });
             dispatch(reviews_fetched({ reviews: serialized_reviews }));
           },
           error => {
@@ -82,8 +84,8 @@ export const fetch_service = (service_id) => {
         const serialized_services = fetch_helpers.mapServiceObjects(json_service);
 
         // Associated Pictures
-        let pics_query = fetch_helpers.build_query("ServicePicture");
-        pics_query.equalTo("service", response[0]);
+        let pics_query = fetch_helpers.build_query('ServicePicture');
+        pics_query.equalTo('service', response[0]);
 
         pics_query.find().then(service_pictures => {
           const service_pics = fetch_helpers.normalizeParseResponseData(service_pictures);
@@ -101,7 +103,6 @@ export const fetch_service = (service_id) => {
     );
   };
 };
-
 
 export const fetchMyTrips = () => async (dispatch, getState) => {
   const state = getState();
@@ -169,10 +170,10 @@ export const setAlreadyAddedToTrip = trip => dispatch => {
   setTimeout(() => dispatch({ type: 'SERVICE_ALREADY_ADDED_TO_TRIP', payload: undefined }), 10000); // 10 s
 };
 
-export const toggleServiceAvailabilitymodal = (bool) => {
+export const toggleServiceAvailabilitymodal = bool => {
   return dispatch => {
     dispatch(set_service_unavailability_modal(false));
-  }
+  };
 };
 
 export const checkAvailability = (serviceId, slotsNb) => {
@@ -182,13 +183,13 @@ export const checkAvailability = (serviceId, slotsNb) => {
       const json_service = fetch_helpers.normalizeParseResponseData(response);
       const serialized_service = fetch_helpers.mapServiceObjects([json_service])[0];
       const service_slots = serialized_service.slots;
-      if(service_slots < slotsNb){
+      if (service_slots < slotsNb) {
         dispatch(set_service_unavailability_modal(true));
-      }else{
+      } else {
         dispatch(set_service_unavailability_modal(false));
       }
-    })
-  }
+    });
+  };
 };
 
 export const fetchServiceContractABI = () => async dispatch => {
