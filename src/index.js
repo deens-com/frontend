4e83f7442b213f2f2d26d62cd4a18c3e57c77e04
Jaydeep Solanki <jaydp17@gmail.com>
2018-07-12 18:39:59 +0530

    if while loading page it contains customer_id identify that user

--- src/index.js
+++ src/index.js
@@ -14,12 +14,27 @@ Parse.initialize('myAppId');
 if (process.env.REACT_APP_NODE_ENV === 'production') {
   Raven.config('https://fd51482cf40f43fca379bc14417b6f2b@sentry.io/1220761').install();
   Parse.serverURL = process.env.REACT_APP_PARSE_SERVER_URL || 'https://internal-api.please.com/parse';
-}else if (process.env.REACT_APP_NODE_ENV === 'staging') {
+} else if (process.env.REACT_APP_NODE_ENV === 'staging') {
   Raven.config('https://fd51482cf40f43fca379bc14417b6f2b@sentry.io/1220761').install();
   Parse.serverURL = process.env.REACT_APP_PARSE_SERVER_URL || 'https://please-api.herokuapp.com/parse';
-}else{
+} else {
   Parse.serverURL = 'https://api.please.docker/parse';
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
 unregisterServiceWorker();
+
+// Stole from: https://stackoverflow.com/a/9870540/1115059
+function getQueryStringValue(key) {
+  return decodeURIComponent(
+    window.location.search.replace(
+      new RegExp('^(?:.*[&\\?]' + encodeURIComponent(key).replace(/[\.\+\*]/g, '\\$&') + '(?:\\=([^&]*))?)?.*$', 'i'),
+      '$1'
+    )
+  );
+}
+
+const customerId = getQueryStringValue('customer_id');
+if (customerId) {
+  window.analytics.identify(customerId);
+}
