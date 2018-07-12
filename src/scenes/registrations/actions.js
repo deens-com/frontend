536bfd5a5b3c4d3c90fa4b0928f37dfe6bb5be77
Jaydeep Solanki <jaydp17@gmail.com>
2018-07-12 14:48:31 +0530

    format

--- src/scenes/registrations/actions.js
+++ src/scenes/registrations/actions.js
@@ -1,39 +1,39 @@
-import Parse from 'parse'
-import history from './../../main/history'
+import Parse from 'parse';
+import history from './../../main/history';
 
 export const types = {
   REGISTRATION_SUCCESS: 'REGISTRATION_SUCCESS',
-  REGISTRATION_FAILED: 'REGISTRATION_FAILED'
+  REGISTRATION_FAILED: 'REGISTRATION_FAILED',
 };
 
-export const registrationSuccess = (session) => {
+export const registrationSuccess = session => {
   return {
     type: this.types.REGISTRATION_SUCCESS,
-    payload: session
-  }
-}
+    payload: session,
+  };
+};
 
-export const registrationFailed = (error_payload) => {
+export const registrationFailed = error_payload => {
   return {
     type: this.types.REGISTRATION_FAILED,
-    payload: error_payload
-  }
-}
+    payload: error_payload,
+  };
+};
 
 export const postRegistration = (username, email, password) => {
-  return (dispatch) => {
-    let user = new Parse.User()
-    user.set('username', username)
-    user.set('email', email)
-    user.set('password', password)
+  return dispatch => {
+    let user = new Parse.User();
+    user.set('username', username);
+    user.set('email', email);
+    user.set('password', password);
     user.signUp(null, {
-      success: (user) => {
-        dispatch(registrationSuccess({session: user}))
-        history.push('/')
+      success: user => {
+        dispatch(registrationSuccess({ session: user }));
+        history.push('/');
       },
       error: (user, error) => {
-        dispatch(registrationFailed({user: user, error: error}))
-      }
-    })
-  }
-}
+        dispatch(registrationFailed({ user: user, error: error }));
+      },
+    });
+  };
+};
