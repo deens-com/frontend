cc2fc21c3bc6d4d2df1bb7dc3068b00c9c2aed60
Olivier Ducroux <olivier@dmates.io>
2018-07-16 12:51:52 +0700

    removing db_schema.js

--- db_schema.js
+++
@@ -1,7 +0,0 @@
-const Parse = require("parse/node").Parse;
-Parse.serverURL = "http://please-api.herokuapp.com/parse";
-Parse.initialize("myAppId", "RESTKEY", "myMasterKey");
-
-Parse.Schema.all().then(res => {
-  console.log(res);
-});
