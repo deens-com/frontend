cfa15951ec75a756e606f83033c11db4e7db8ce8
Olivier Ducroux <olivier@dmates.io>
2018-07-16 12:51:25 +0700

    removing repl.js

--- repl.js
+++
@@ -1,8 +0,0 @@
-const repl = require("repl");
-const replServer = repl.start({ prompt: "my-app > " });
-const Parse = require("parse/node").Parse;
-
-Parse.serverURL = "http://api.please.docker/parse";
-Parse.initialize("myAppId", "RESTKEY", "myMasterKey");
-
-replServer.context.Parse = Parse;
