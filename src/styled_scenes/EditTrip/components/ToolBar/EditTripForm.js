73cf6410c8de7ae35115642fb64828d9f8165f2c
dm0lz <ducrouxolivier@gmail.com>
2018-07-27 11:59:53 +0200

    increasing timeout

--- src/styled_scenes/EditTrip/components/ToolBar/EditTripForm.js
+++ src/styled_scenes/EditTrip/components/ToolBar/EditTripForm.js
@@ -141,7 +141,7 @@ class EditTripForm extends Component {
               value={values.tags}
               onChange={(e, { name, value }) => {
                 this.props.setFieldValue('tags', value);
-                setTimeout(submitForm, 1000);
+                setTimeout(submitForm, 2000);
               }}
             />
           </Form.Field>
