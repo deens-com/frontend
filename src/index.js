af4983958ac63c28bf60ee08784c92165f26175e
Jaydeep Solanki <jaydp17@gmail.com>
2018-08-07 18:31:10 +0530

    use config to store server base path

--- src/index.js
+++ src/index.js
@@ -9,22 +9,18 @@ import createHistory from 'history/createBrowserHistory';
 import { unregister as unregisterServiceWorker } from './registerServiceWorker';
 import Parse from 'parse';
 import Raven from 'raven-js';
+import { serverBaseURL, isProd, isStaging } from './libs/config';
 
 Parse.initialize('myAppId');
+Parse.serverURL = `${serverBaseURL}/parse`;
+
 const history = createHistory();
 
-if (process.env.REACT_APP_NODE_ENV === 'production') {
+if (isProd || isStaging) {
   Raven.config('https://fd51482cf40f43fca379bc14417b6f2b@sentry.io/1220761').install();
-  Parse.serverURL = process.env.REACT_APP_PARSE_SERVER_URL || 'https://api.please.com/parse';
-} else if (process.env.REACT_APP_NODE_ENV === 'staging') {
-  Raven.config('https://fd51482cf40f43fca379bc14417b6f2b@sentry.io/1220761').install();
-  Parse.serverURL =
-    process.env.REACT_APP_PARSE_SERVER_URL || 'https://staging-api.please.com/parse';
-} else {
-  Parse.serverURL = process.env.REACT_APP_PARSE_SERVER_URL || 'https://api.please.docker/parse';
 }
 
-if (process.env.NODE_ENV === 'production') {
+if (isProd) {
   const noop = () => {};
   const error = error => Raven.captureException(error);
   console.log = noop;
