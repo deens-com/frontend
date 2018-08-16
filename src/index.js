74e9753176462e1ee71ddef6bc67b30c254d6241
Jaydeep Solanki <jaydp17@gmail.com>
2018-08-16 14:37:32 +0530

    use api.please.com & staging-api.please.com

--- src/index.js
+++ src/index.js
@@ -15,12 +15,11 @@ const history = createHistory();
 
 if (process.env.REACT_APP_NODE_ENV === 'production') {
   Raven.config('https://fd51482cf40f43fca379bc14417b6f2b@sentry.io/1220761').install();
-  Parse.serverURL =
-    process.env.REACT_APP_PARSE_SERVER_URL || 'https://internal-api.please.com/parse';
+  Parse.serverURL = process.env.REACT_APP_PARSE_SERVER_URL || 'https://api.please.com/parse';
 } else if (process.env.REACT_APP_NODE_ENV === 'staging') {
   Raven.config('https://fd51482cf40f43fca379bc14417b6f2b@sentry.io/1220761').install();
   Parse.serverURL =
-    process.env.REACT_APP_PARSE_SERVER_URL || 'https://staging-internal-api.please.com/parse';
+    process.env.REACT_APP_PARSE_SERVER_URL || 'https://staging-api.please.com/parse';
 } else {
   Parse.serverURL = process.env.REACT_APP_PARSE_SERVER_URL || 'https://api.please.docker/parse';
 }
