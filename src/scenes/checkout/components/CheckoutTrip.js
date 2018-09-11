60a860b0abf510e48dffc04b67b04ea20f18c8fb
Jaydeep Solanki <jaydp17@gmail.com>
2018-09-12 02:55:58 +0530

    make checkout page display the trip & it's services

--- src/scenes/checkout/components/CheckoutTrip.js
+++ src/scenes/checkout/components/CheckoutTrip.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TripDay from './TripDay';
+import I18nText from 'shared_components/I18nText';
 
-const CheckoutTrip = ({ trip, scheduledServices }) => {
-  const tripDays = scheduledServices.map(({ day, services }) => (
-    <TripDay key={day} tripBeginDate={trip.beginDate} dayIndex={day} services={services} />
+const CheckoutTrip = ({ trip, servicesGroupedByDay }) => {
+  const tripDays = servicesGroupedByDay.map(({ day, services }) => (
+    <TripDay key={day} tripBeginDate={trip.startDate} dayIndex={day} services={services} />
   ));
   return (
     <div>
       <p>Your trip details are provided below. Please check them and finalize the payment</p>
       <hr />
-      <h3>{trip && trip.title}</h3>
+      <h3>
+        <I18nText data={trip.title} />
+      </h3>
       {tripDays}
     </div>
   );
@@ -18,7 +21,7 @@ const CheckoutTrip = ({ trip, scheduledServices }) => {
 
 CheckoutTrip.propTypes = {
   trip: PropTypes.object.isRequired,
-  scheduledServices: PropTypes.arrayOf(PropTypes.object).isRequired,
+  servicesGroupedByDay: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default CheckoutTrip;
