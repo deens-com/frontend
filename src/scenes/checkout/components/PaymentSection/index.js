c65cb67bd661273a0aa2a4aee41ea7d7faf7ca2e
Jaydeep Solanki <jaydp17@gmail.com>
2018-09-12 17:15:59 +0530

    remove stripe feature flag

--- src/scenes/checkout/components/PaymentSection/index.js
+++ src/scenes/checkout/components/PaymentSection/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Label, Grid } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import styled from 'styled-components';
 
 import PriceTag from 'shared_components/Currency/PriceTag';
@@ -8,7 +8,6 @@ import { media } from 'libs/styled';
 import BookedSuccessfullyPopup from '../BookedSuccessfullyPopup';
 import StripeAutoPaymentButton from '../StripeAutoPaymentButton';
 import StripeCardDetails from '../StripeCardDetails';
-import { isStripeIntegrationEnabled } from 'libs/feature-flags';
 
 const Wrap = styled.div`
   ${media.minSmall} {
@@ -44,7 +43,6 @@ export default class PaymentSection extends Component {
   };
 
   state = {
-    showStripeIntegration: isStripeIntegrationEnabled(),
     canMakeAutoPayment: false,
   };
 
@@ -56,7 +54,6 @@ export default class PaymentSection extends Component {
     const {
       pricePerPerson,
       totalPrice,
-      onPaymentClick,
       numberOfPerson,
       isLoading,
       onStripeTokenReceived,
@@ -83,69 +80,42 @@ export default class PaymentSection extends Component {
               <PriceTag price={totalPrice} unit="hidden" />
             </Grid.Column>
           </Grid.Row>
-          {!this.state.showStripeIntegration && (
-            <Grid.Row columns={1} textAlign="right">
-              <Grid.Column>
-                <PriceTag price={totalPrice}>
-                  {({ convertedPrice, symbol }) => (
-                    <Button
-                      as="div"
-                      labelPosition="right"
-                      onClick={onPaymentClick}
-                      disabled={parseFloat(convertedPrice) === 0}
-                    >
-                      <Button color="green" loading={isLoading}>
-                        Pay
-                      </Button>
-                      <Label as="a" basic color="green" pointing="left">
-                        <PriceTag.PriceStyle>
-                          {symbol}
-                          {convertedPrice}
-                        </PriceTag.PriceStyle>
-                      </Label>
-                    </Button>
-                  )}
-                </PriceTag>
-              </Grid.Column>
-            </Grid.Row>
-          )}
-          {this.state.showStripeIntegration && (
-            <Grid.Row>
-              <Grid.Column>
-                {totalPrice && (
-                  <StripWrap>
-                    <PriceTag price={totalPrice}>
-                      {({ convertedPrice, stripeMultiplier, selectedCurrency, symbol }) => {
-                        const amount = parseFloat(convertedPrice);
-                        return (
-                          <React.Fragment>
-                            <StripeAutoPaymentButton
-                              key={selectedCurrency}
-                              amount={amount}
-                              stripeMultiplier={stripeMultiplier}
-                              currency={selectedCurrency}
-                              onStripeTokenReceived={onStripeTokenReceived}
-                              canMakeAutoPayment={this.setCanMakeAutoPayment}
-                            />
-                            {paymentError && (
-                              <ErrorMessage>
-                                {paymentError.customMessage || paymentError.message}
-                              </ErrorMessage>
-                            )}
-                            <StripeCardDetails
-                              amount={amount}
-                              symbol={symbol}
-                              showOrInText={this.state.canMakeAutoPayment}
-                            />
-                          </React.Fragment>
-                        );
-                      }}
-                    </PriceTag>
-                  </StripWrap>
-                )}
-              </Grid.Column>
-            </Grid.Row>
-          )}
+
+          <Grid.Row>
+            <Grid.Column>
+              {totalPrice && (
+                <StripWrap>
+                  <PriceTag price={totalPrice}>
+                    {({ convertedPrice, stripeMultiplier, selectedCurrency, symbol }) => {
+                      const amount = parseFloat(convertedPrice);
+                      return (
+                        <React.Fragment>
+                          <StripeAutoPaymentButton
+                            key={selectedCurrency}
+                            amount={amount}
+                            stripeMultiplier={stripeMultiplier}
+                            currency={selectedCurrency}
+                            onStripeTokenReceived={onStripeTokenReceived}
+                            canMakeAutoPayment={this.setCanMakeAutoPayment}
+                          />
+                          {paymentError && (
+                            <ErrorMessage>
+                              {paymentError.customMessage || paymentError.message}
+                            </ErrorMessage>
+                          )}
+                          <StripeCardDetails
+                            amount={amount}
+                            symbol={symbol}
+                            showOrInText={this.state.canMakeAutoPayment}
+                          />
+                        </React.Fragment>
+                      );
+                    }}
+                  </PriceTag>
+                </StripWrap>
+              )}
+            </Grid.Column>
+          </Grid.Row>
         </Grid>
         <BookedSuccessfullyPopup />
       </Wrap>
