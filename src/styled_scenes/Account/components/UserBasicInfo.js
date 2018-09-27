6d0c4d19a3be5cd894917ba60bf1de3f2cb08900
dm0lz <ducrouxolivier@gmail.com>
2018-09-27 19:33:31 +0200

    update user avatar

--- src/styled_scenes/Account/components/UserBasicInfo.js
+++ src/styled_scenes/Account/components/UserBasicInfo.js
@@ -55,7 +55,7 @@ const FileInputWrapper = styled.div`
 
 const UserBasicInfo = ({ user_profile: user = {}, match, update_user_avatar, logOut }) => {
   const name = user.fullName || user.username;
-  const dpUrl = (user.profilePicture && user.profilePicture.url) || ImgurAvatar;
+  const dpUrl = (user.profilePicture && user.profilePicture.url) || user.metamaskPublicAddress || ImgurAvatar;
   let activePath = match.path.replace('/account/', '');
   // const logout = () => {
   //   props.logOut();
