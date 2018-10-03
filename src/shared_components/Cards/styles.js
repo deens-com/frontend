c94898490e1313f25698eb477aabb6102a61e5c7
Vladimir Kokovic <vlada.kokovic@gmail.com>
2018-10-03 12:29:41 +0200

    fix cards on search results based on service typees

--- src/shared_components/Cards/styles.js
+++ src/shared_components/Cards/styles.js
@@ -4,7 +4,7 @@ import { media } from '../../libs/styled';
 
 export const ContentWrap = styled.div`
   padding: 15px;
-  height: 180px;
+  height: ${props => (props.small ? '100px' : '180px')};
 
   ${media.minSmall} {
     padding: 20px;
