1d8be617a0537caa54093ee6e2152302f389d353
Jaydeep Solanki <jaydp17@gmail.com>
2018-11-16 15:39:21 +0530

    use user.plsBalance.$numberDecimal only if $numberDecimal exists

--- src/libs/fetch_helpers.js
+++ src/libs/fetch_helpers.js
@@ -250,7 +250,9 @@ const buildServicesJson = services => {
 };
 
 const buildUserJson = user => {
-  user.plsBalance = user.plsBalance.$numberDecimal;
+  if (user.plsBalance.$numberDecimal) {
+    user.plsBalance = user.plsBalance.$numberDecimal;
+  }
   return user;
 };
 
