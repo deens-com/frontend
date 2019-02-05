abbb0f2d5fc4dbddc1109b9a61056eababa14f8b
fcmatteo <facumatteo@gmail.com>
2019-02-04 22:32:13 -0300

    Fix trip organizer bug

--- cypress/integration/trip-organizer/guest.spec.js
+++ cypress/integration/trip-organizer/guest.spec.js
@@ -1,4 +1,6 @@
 import moment from 'moment';
+import tripResponse from './trip-response.json';
+import guestTrip from './trip-guest.json';
 
 describe('Should go to trip organizer', function() {
   before(() => {
@@ -27,11 +29,7 @@ describe('Should go to trip organizer', function() {
       cy.root()
         .find('input')
         .should('have.length', 2);
-      cy.log(
-        moment()
-          .add('days', 1)
-          .format('MM/DD/YY'),
-      );
+
       cy.root()
         .get('input:enabled')
         .should(
@@ -49,5 +47,38 @@ describe('Should go to trip organizer', function() {
             .format('MM/DD/YY'),
         );
     });
+  });
+});
+
+describe('Should login to book the trip', function() {
+  const getTripUrl = `/trips/${tripResponse._id}?include=services,tags,reservations`;
+
+  before(() => {
+    //const trip = JSON.(tripResponse)
+    /*cy.server()
+    cy.route({
+      method: 'GET',
+      url: getTripUrl,
+      response: tripResponse,
+    })*/
+    window.localStorage.setItem(
+      `please-${Cypress.env('NODE_ENV')}-anonymous-trip`,
+      JSON.stringify(guestTrip),
+    );
+  });
+
+  beforeEach(() => {
+    cy.visit(`/trips/organize`);
+  });
+
+  after(() => {
+    window.localStorage.removeItem(`please-${Cypress.env('NODE_ENV')}-anonymous-trip`);
+  });
+
+  it('Get id', function() {
+    cy.findTestElement('day').should('have.length', 3);
+
+    cy.getTestElement('checkoutBookButton').click();
+    cy.url().should('include', '/register');
   });
 });
