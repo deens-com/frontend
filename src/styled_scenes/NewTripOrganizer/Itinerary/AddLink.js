de6a32c1079ba19554d4b6c101e93d4407d461e3
Facundo Matteo <facumatteo@gmail.com>
2019-04-02 17:20:32 -0300

    Handle error when pasting url

--- src/styled_scenes/NewTripOrganizer/Itinerary/AddLink.js
+++ src/styled_scenes/NewTripOrganizer/Itinerary/AddLink.js
@@ -1,11 +1,14 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef } from 'react';
 import { Modal, Dimmer, Loader } from 'semantic-ui-react';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import axios from 'libs/axios';
 import Input from 'shared_components/StyledInput';
 import Button from 'shared_components/Button';
 import fetchHelpers from 'libs/fetch_helpers';
 import { parseLocationDataAndCoordinates } from 'libs/location';
+import * as colors from 'libs/colors';
+
+const { error: errorColor } = colors;
 
 const PasteUrlMessage = styled.div`
   font-weight: bold;
@@ -26,69 +29,77 @@ const CancelButton = styled.div`
   cursor: pointer;
 `;
 
+const ErrorMsg = styled.div`
+  color: ${errorColor};
+  margin-top: 5px;
+`;
+
 const AddCustomServiceModal = ({ close, setServiceData }) => {
-  const ref = useRef(null)
-  const [isFetching, setIsFetching] = useState(false)
-  let geocoder
+  const ref = useRef(null);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState('');
+  let geocoder;
   useEffect(() => {
-    geocoder = new window.google.maps.Geocoder()
+    geocoder = new window.google.maps.Geocoder();
   }, []);
 
   const fetchUrlData = async () => {
     const url = ref.current.value;
-    setIsFetching(true)
-    const metadata = (await axios.post('/links/extract', { url })).data;
+    setIsFetching(true);
+    setError('');
+    try {
+      const metadata = (await axios.post('/links/extract', { url })).data;
+      if (metadata.location) {
+        const latlng = {
+          lat: parseFloat(metadata.location.latitude),
+          lng: parseFloat(metadata.location.longitude),
+        };
 
-    if (metadata.location) {
-      const latlng = {
-        lat: parseFloat(metadata.location.latitude),
-        lng: parseFloat(metadata.location.longitude),
-      };
-
-      geocoder.geocode({ location: latlng }, (results, status) => {
-        if (status === 'OK') {
-          if (results[0]) {
-            setServiceData(
-              fetchHelpers.buildServiceForView(
-                fetchHelpers.createServiceFromUrl({
-                  ...metadata,
-                  location: parseLocationDataAndCoordinates(results[0], [
-                    latlng.lng,
-                    latlng.lat,
-                  ]),
-                }),
-              )
-            )
-            return;
+        geocoder.geocode({ location: latlng }, (results, status) => {
+          if (status === 'OK') {
+            if (results[0]) {
+              setServiceData(
+                fetchHelpers.buildServiceForView(
+                  fetchHelpers.createServiceFromUrl({
+                    ...metadata,
+                    location: parseLocationDataAndCoordinates(results[0], [latlng.lng, latlng.lat]),
+                  }),
+                ),
+              );
+              return;
+            }
           }
-        }
 
-        setServiceData(
-          fetchHelpers.buildServiceForView(
-            fetchHelpers.createServiceFromUrl(metadata),
-          )
-        )
-      });
-      return;
+          setServiceData(
+            fetchHelpers.buildServiceForView(fetchHelpers.createServiceFromUrl(metadata)),
+          );
+        });
+        return;
+      }
+      setServiceData(fetchHelpers.buildServiceForView(fetchHelpers.createServiceFromUrl(metadata)));
+    } catch (e) {
+      if (e.response && e.response.status === 400 && e.response.data) {
+        const err = e.response.data.message;
+        setError(err.charAt(0).toUpperCase() + err.substr(1));
+      }
+    } finally {
+      setIsFetching(false);
     }
-    setServiceData(fetchHelpers.buildServiceForView(fetchHelpers.createServiceFromUrl(metadata)))
-  }
+  };
 
   return (
     <>
       <Modal.Header>Add a link to create a service</Modal.Header>
       <Modal.Content>
         <Dimmer.Dimmable>
-          <Dimmer
-            inverted
-            active={isFetching}
-          >
+          <Dimmer inverted active={isFetching}>
             <Loader />
           </Dimmer>
           <PasteUrlMessage>
             Copy/Paste here the link to the service you want to add in your trip
           </PasteUrlMessage>
           <Input innerRef={ref} />
+          {error && <ErrorMsg>{error}</ErrorMsg>}
           <Buttons>
             <Button onClick={fetchUrlData}>Next</Button>
             <CancelButton onClick={close}>Cancel</CancelButton>
@@ -96,7 +107,7 @@ const AddCustomServiceModal = ({ close, setServiceData }) => {
         </Dimmer.Dimmable>
       </Modal.Content>
     </>
-  )
-}
+  );
+};
 
-export default AddCustomServiceModal
+export default AddCustomServiceModal;
