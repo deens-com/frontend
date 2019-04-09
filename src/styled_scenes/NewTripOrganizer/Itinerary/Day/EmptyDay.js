b1ab0f705d6d842f7d058c3d8c1a18417408699c
Facundo Matteo <facumatteo@gmail.com>
2019-04-09 08:39:55 -0300

    Add EmptyDay component

---
+++ src/styled_scenes/NewTripOrganizer/Itinerary/Day/EmptyDay.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import AddServiceBox from './AddServiceBox';
+import DayTitle from './DayTitle';
+
+const Wrapper = styled.div`
+  position: relative;
+  display: inline-block;
+  margin-bottom: 25px;
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  top: 0;
+  background-color: white;
+  opacity: 0.75;
+  z-index: 1;
+  cursor: pointer;
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px;
+`;
+
+const EmptyDay = ({ addNewDay, day, tripStartDate }) => {
+  const add = () => addNewDay();
+  return (
+    <div>
+      <Wrapper>
+        <Overlay onClick={add} />
+        <TitleWrapper>
+          <DayTitle day={day} tripStartDate={tripStartDate} />
+        </TitleWrapper>
+        <AddServiceBox lowMargin />
+      </Wrapper>
+    </div>
+  );
+};
+
+export default EmptyDay;
