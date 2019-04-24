e1331727b27d1111936c9e17cf4eb464ec8259a4
Facundo Matteo <facumatteo@gmail.com>
2019-04-24 12:20:14 -0300

    Change currency dropdown

--- src/shared_components/HelpMe/CurrencyDropdownContent.js
+++ src/shared_components/HelpMe/CurrencyDropdownContent.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
-import { primary, primaryHover, textLight } from 'libs/colors';
+import { disabled } from 'libs/colors';
 
 const Element = styled.div`
-  min-width: 200px;
+  min-width: 325px;
   cursor: pointer;
   padding: 5px 10px;
-  color: ${textLight};
-  background-color: ${primary};
-  &:nth-child(odd) {
-    background-color: ${primaryHover};
+  background-color: white;
+  display: flex;
+  font-style: italic;
+  &:hover {
+    background-color: ${disabled};
   }
+`;
+
+const Code = styled.span`
+  font-weight: bold;
+  width: 45px;
+  font-style: normal;
 `;
 
 export default ({ closeDropdown, currencies, setCurrency }) =>
@@ -22,6 +29,6 @@ export default ({ closeDropdown, currencies, setCurrency }) =>
         closeDropdown();
       }}
     >
-      {key} - {currencies[key]}
+      <Code>{key}</Code> {currencies[key]}
     </Element>
   ));
