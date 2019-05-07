79514d52c2d1ed4339f1236be8110763f4ee0717
Facundo Matteo <facumatteo@gmail.com>
2019-05-07 09:28:02 -0300

    Fix styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import 'semantic-ui-css/semantic.min.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import App from './main/app';
