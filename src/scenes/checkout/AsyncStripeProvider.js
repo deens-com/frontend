072aa63913bdbe3f2e6a8bbd8e7ce503ba11a8ca
Jaydeep Solanki <jaydp17@gmail.com>
2019-05-17 15:02:01 +0530

    load stripe only on the payment page

---
+++ src/scenes/checkout/AsyncStripeProvider.js
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { StripeProvider } from 'react-stripe-elements';
+
+// Copied from https://github.com/stripe/react-stripe-elements/issues/154#issuecomment-462554749
+export default class AsyncStripeProvider extends Component {
+  static propTypes = {
+    apiKey: PropTypes.string.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      stripe: null,
+    };
+  }
+
+  componentDidMount() {
+    this._mounted = true;
+    const { apiKey } = this.props;
+
+    const stripeJs = document.createElement('script');
+    stripeJs.src = 'https://js.stripe.com/v3/';
+    stripeJs.async = true;
+    stripeJs.onload = () => {
+      if (this._mounted) {
+        this.setState({
+          stripe: window.Stripe(apiKey),
+        });
+      }
+    };
+    document.body && document.body.appendChild(stripeJs);
+  }
+
+  componentWillUnmount() {
+    this._mounted = false;
+  }
+
+  render() {
+    const { stripe } = this.state;
+
+    return (
+      <StripeProvider stripe={stripe}>
+        <>{this.props.children}</>
+      </StripeProvider>
+    );
+  }
+}
