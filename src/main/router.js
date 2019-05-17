6b8fd2994cc57bc1cd292c5b7cc5b47ac32c5e7e
Jaydeep Solanki <jaydp17@gmail.com>
2019-05-17 17:48:40 +0530

    try React.lazy for most of the routes

--- src/main/router.js
+++ src/main/router.js
@@ -3,26 +3,10 @@ import { Switch } from 'react-router';
 import { Route } from 'react-router-dom';
 import withErrorBoundary from './middlewares/WithErrorBoundary';
 import Home from './../scenes/home/home';
-import EarnMoney from './../scenes/earn-money';
-import TokenSale from './../scenes/token-sale';
-import UserVerification from '../scenes/user-verification';
-import CookiePolicy from './../scenes/cookie-policy';
-import Account from './../scenes/account/account';
-import Sessions from './../scenes/sessions/sessions';
-import Results from './../scenes/results/results';
-import Trip from './../scenes/trip';
-import TripCreator from './../scenes/trip-creator';
-import TripOrganizer from './../scenes/trip-organizer';
-import TripShare from './../scenes/trip-share';
-import Users from './../scenes/users/users';
-import Services from './../scenes/services/services';
-import Registrations from './../scenes/registrations/registrations';
-import RecoverPassword from './../scenes/recover-password';
+
 import Notfound from './../styled_scenes/NotFound';
 import ScrollToTop from './middlewares/ScrollToTop';
-import ServiceUpsert from '../scenes/service-upsert';
-import Checkout from '../scenes/checkout';
-import BlogPost from '../scenes/blog';
+
 import PrivateRoute from './PrivateRoute';
 import OnlyPublicRoute from './OnlyPublicRoute';
 import withSegmentTracker from './middlewares/with_segment_tracker';
@@ -33,65 +17,92 @@ export default (
   <ScrollToTop>
     <Switch>
       <Route exact path={process.env.PUBLIC_URL + '/'} component={commonHOCs(Home)} />
-      <OnlyPublicRoute path={process.env.PUBLIC_URL + '/login'} component={commonHOCs(Sessions)} />
+      <OnlyPublicRoute
+        path={process.env.PUBLIC_URL + '/login'}
+        component={commonHOCs(React.lazy(() => import('./../scenes/sessions/sessions')))}
+      />
       <Route
         path={process.env.PUBLIC_URL + '/user-verification'}
-        component={commonHOCs(UserVerification)}
+        component={commonHOCs(React.lazy(() => import('../scenes/user-verification')))}
       />
       <OnlyPublicRoute
         path={process.env.PUBLIC_URL + '/register'}
-        component={commonHOCs(Registrations)}
+        component={commonHOCs(React.lazy(() => import('./../scenes/registrations/registrations')))}
       />
       <OnlyPublicRoute
         path={process.env.PUBLIC_URL + '/recover-password'}
-        component={commonHOCs(RecoverPassword)}
+        component={commonHOCs(React.lazy(() => import('./../scenes/recover-password')))}
       />
-      <Route path={process.env.PUBLIC_URL + '/earn-money'} component={commonHOCs(EarnMoney)} />
-      <Route path={process.env.PUBLIC_URL + '/token-sale'} component={commonHOCs(TokenSale)} />
+      <Route
+        path={process.env.PUBLIC_URL + '/earn-money'}
+        component={commonHOCs(React.lazy(() => import('./../scenes/earn-money')))}
+      />
+      <Route
+        path={process.env.PUBLIC_URL + '/token-sale'}
+        component={commonHOCs(React.lazy(() => import('./../scenes/token-sale')))}
+      />
       <Route
         path={process.env.PUBLIC_URL + '/token-sale/smart-contract'}
-        component={commonHOCs(TokenSale)}
+        component={commonHOCs(React.lazy(() => import('./../scenes/token-sale')))}
       />
       <Route
         path={process.env.PUBLIC_URL + '/cookie-policy'}
-        component={commonHOCs(CookiePolicy)}
+        component={commonHOCs(React.lazy(() => import('./../scenes/cookie-policy')))}
       />
-      <Route path={process.env.PUBLIC_URL + '/results'} component={commonHOCs(Results)} />
+      <Route
+        path={process.env.PUBLIC_URL + '/results'}
+        component={commonHOCs(React.lazy(() => import('./../scenes/results/results')))}
+      />
       <PrivateRoute
         path={process.env.PUBLIC_URL + '/services/new'}
-        component={commonHOCs(ServiceUpsert)}
+        component={commonHOCs(React.lazy(() => import('../scenes/service-upsert')))}
       />
       <PrivateRoute
         path={process.env.PUBLIC_URL + '/services/edit/:id'}
-        component={commonHOCs(ServiceUpsert)}
+        component={commonHOCs(React.lazy(() => import('../scenes/service-upsert')))}
       />
       <Route
         path={process.env.PUBLIC_URL + '/services/:slug?_:id'}
-        component={commonHOCs(Services)}
+        component={commonHOCs(React.lazy(() => import('./../scenes/services/services')))}
       />
       <Route
         path={process.env.PUBLIC_URL + '/trips/organize/:id'}
-        component={commonHOCs(TripOrganizer)}
+        component={commonHOCs(React.lazy(() => import('./../scenes/trip-organizer')))}
         message="Please login or register to continue with your trip."
       />
       <Route
         path={process.env.PUBLIC_URL + '/trips/organize'}
-        component={commonHOCs(TripOrganizer)}
+        component={commonHOCs(React.lazy(() => import('./../scenes/trip-organizer')))}
       />
       <PrivateRoute
         path={process.env.PUBLIC_URL + '/trips/share/:id'}
-        component={commonHOCs(TripShare)}
+        component={commonHOCs(React.lazy(() => import('./../scenes/trip-share')))}
         message="Please login or register to share your trip."
       />
       <Route
         path={process.env.PUBLIC_URL + '/trips/checkout/:id'}
-        component={commonHOCs(Checkout)}
+        component={commonHOCs(React.lazy(() => import('../scenes/checkout')))}
       />
-      <Route path={process.env.PUBLIC_URL + '/trips/create'} component={commonHOCs(TripCreator)} />
-      <Route path={process.env.PUBLIC_URL + '/trips/:slug?_:id'} component={commonHOCs(Trip)} />
-      <Route path={process.env.PUBLIC_URL + '/users/:userName'} component={commonHOCs(Users)} />
-      <Route path={process.env.PUBLIC_URL + '/account'} component={commonHOCs(Account)} />
-      <Route path={process.env.PUBLIC_URL + '/:slug'} component={commonHOCs(BlogPost)} />
+      <Route
+        path={process.env.PUBLIC_URL + '/trips/create'}
+        component={commonHOCs(React.lazy(() => import('./../scenes/trip-creator')))}
+      />
+      <Route
+        path={process.env.PUBLIC_URL + '/trips/:slug?_:id'}
+        component={commonHOCs(React.lazy(() => import('./../scenes/trip')))}
+      />
+      <Route
+        path={process.env.PUBLIC_URL + '/users/:userName'}
+        component={commonHOCs(React.lazy(() => import('./../scenes/users/users')))}
+      />
+      <Route
+        path={process.env.PUBLIC_URL + '/account'}
+        component={commonHOCs(React.lazy(() => import('./../scenes/account/account')))}
+      />
+      <Route
+        path={process.env.PUBLIC_URL + '/:slug'}
+        component={commonHOCs(React.lazy(() => import('../scenes/blog')))}
+      />
       <Route component={withErrorBoundary(Notfound)} />
     </Switch>
   </ScrollToTop>
