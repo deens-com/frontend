fd9eaae7896b5cdb4087f96667c9520d73fb5952
Facundo Matteo <facumatteo@gmail.com>
2019-06-04 13:46:39 +0200

    Add PREMIUM to modal

--- src/main/middlewares/ScrollToTop.js
+++ src/main/middlewares/ScrollToTop.js
@@ -5,7 +5,7 @@ import { withRouter } from 'react-router-dom';
 
 class ScrollToTop extends Component {
   componentDidUpdate(prevProps) {
-    if (this.props.location !== prevProps.location) {
+    if (this.props.location !== prevProps.location && !this.props.dontScroll) {
       window.scrollTo(0, 0);
     }
   }
